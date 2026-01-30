a6c956994f37eac06dde9a28028e8a915576e6f1
archero34 <yjmurph@gmail.com>
2026-01-30 01:01:06 -0500

    Add files via upload

---
+++ index-CS5tPIud.js
@@ -0,0 +1,1 @@
+const e=[{title:"10",value:10},{title:"25",value:25},{title:"50",value:50},{title:"All",value:1e3}],t=50,s={"Not Started":"primary","In Progress":"info",Completed:"success"};export{t as d,e as i,s};
